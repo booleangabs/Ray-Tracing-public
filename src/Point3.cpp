8ab78fb385ab0490a3f92d2ec719ee25782b9506
Almir Menezes da Cunha Júnior <almir.cunha@incognia.com>
2023-03-21 20:24:01 -0300

    fix(point3): include header

--- src/Point3.cpp
+++ src/Point3.cpp
@@ -1,4 +1,4 @@
-#include "Point3.hpp"
+#include "../include/Point3.hpp"
 
 class Point3
 {
@@ -8,11 +8,11 @@ public:
     Point3() : x(0.0), y(0.0), z(0.0) {}
     Point3(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
 
-     Point3 operator/(double scalar) const {
+    Point3 operator/(double scalar) const {
         return Point3(x/scalar, y/scalar, z/scalar);
     }
 
-     Point3 operator*(double scalar) const {
+    Point3 operator*(double scalar) const {
         return Point3(x*scalar, y*scalar, z*scalar);
     }
 };
