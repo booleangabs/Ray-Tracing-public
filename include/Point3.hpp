85ee78bc632fdb82459b154bc6b84ee39998f6fb
Almir Menezes da Cunha Júnior <almir.cunha@incognia.com>
2023-03-21 20:23:49 -0300

    feat(point3): add operators to header

--- include/Point3.hpp
+++ include/Point3.hpp
@@ -1,17 +1,14 @@
 #ifndef POINT3_H
 #define POINT3_H
 
-namespace rtc {
-    
-    class Point3 {
-        public:
-            double x, y, z;
-
-            Point3();
-            Point3(double _x, double _y, double _z);
-
-    };
-    
-}
+class Point3 {
+    private:
+        double x, y, z;
+    public:
+        Point3();
+        Point3(double _x, double _y, double _z);
+        Point3 operator/(double scalar) const;
+        Point3 operator*(double scalar) const;
+};
 
 #endif
