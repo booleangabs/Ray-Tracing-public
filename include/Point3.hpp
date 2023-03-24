dead9d776f32ffef939993ff9069f024970df859
Gabriel Tavares <62074916+booleangabs@users.noreply.github.com>
2023-03-24 07:44:48 -0300

    feat(point3): Adicionados setters

--- include/Point3.hpp
+++ include/Point3.hpp
@@ -4,12 +4,19 @@
 class Point3 {
     private:
         double x, y, z;
+
     public:
         Point3();
         Point3(double _x, double _y, double _z);
+
         double getX() const;
         double getY() const;
         double getZ() const;
+        void setX(double _x);
+        void setY(double _y);
+        void setZ(double _z);
+        void set(double _x, double _y, double _z);
+
         Point3 operator/(double scalar) const;
         Point3 operator*(double scalar) const;
 };
