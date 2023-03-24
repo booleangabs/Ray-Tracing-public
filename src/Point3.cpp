2cace4c8aa7c351c909fa16a1f58f0b249cd5ea1
Gabriel Tavares <62074916+booleangabs@users.noreply.github.com>
2023-03-24 07:44:48 -0300

    feat(point3): Adicionados setters

--- src/Point3.cpp
+++ src/Point3.cpp
@@ -16,6 +16,24 @@ double Point3::getZ() const {
     return z;
 }
 
+void Point3::setX(double _x) {
+    x = _x;
+}
+
+void Point3::setY(double _y) {
+    y = _y;
+}
+
+void Point3::setZ(double _z) {
+    z = _z;
+}
+
+void Point3::set(double _x, double _y, double _z) {
+    x = _x;
+    y = _y;
+    z = _z;
+}
+
 Point3 Point3::operator*(double scalar) const {
     return Point3(x * scalar, y * scalar, z * scalar);
 }
