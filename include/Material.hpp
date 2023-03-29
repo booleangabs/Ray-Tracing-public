bc10f4c70ecdf441da5ec301dac98b3bbb7a9519
Gabriel Tavares <62074916+booleangabs@users.noreply.github.com>
2023-03-28 21:43:31 -0300

    feat(material): Update dos materiais

--- include/Material.hpp
+++ include/Material.hpp
@@ -6,11 +6,23 @@
 #include "HitRecord.hpp"
 
 class Material {
-public:
-    Material(const Color& c);
-    Color shade(const Ray& ray, const HitRecord& hit);
-private:
-    Color color;
+    private:
+        Color albedo; // the "main" color
+        Vec3 kd, ks, ka, kr, kt;
+        double n;
+
+    public:
+        Material(Color _albedo, Vec3 kd = Vec3(), Vec3 ks = Vec3(), 
+                 Vec3 ka = Vec3(), Vec3 kr = Vec3(), Vec3 kt = Vec3(),
+                 double n = 1.0);
+
+        Color getAlbedo();
+        Vec3 getKd();
+        Vec3 getKs();
+        Vec3 getKa();
+        Vec3 getKr();
+        Vec3 getKt();
+        double getN();
 };
 
 #endif
--- src/Material.cpp
+++ src/Material.cpp
@@ -1,7 +1,34 @@
 #include "../include/Material.hpp"
 
-Material::Material(const Color& c): color(c) {};
+Material::Material(Color _albedo, Vec3 _kd = Vec3(), Vec3 _ks = Vec3(), 
+                 Vec3 _ka = Vec3(), Vec3 _kr = Vec3(), Vec3 _kt = Vec3(),
+                 double _n = 1.0) 
+        : albedo(_albedo), kd(_kd), ks(_ks), ka(_ka), kr(_kr), kt(_kt), n(_n) {};
 
-Color Material::shade(const Ray& ray, const HitRecord& hit) {
-    return color;
-};
+Color Material::getAlbedo() {
+    return albedo;
+}
+
+Vec3 Material::getKd() {
+    return kd;
+}
+
+Vec3 Material::getKs() {
+    return ks;
+}
+
+Vec3 Material::getKa() {
+    return ka;
+}
+
+Vec3 Material::getKr() {
+    return kr;
+}
+
+Vec3 Material::getKt() {
+    return kt;
+}
+
+double Material::getN() {
+    return n;
+}
