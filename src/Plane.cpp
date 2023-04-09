f0bdcbaafbb6c782d791ac039b31d87849cb2f0c
Almir Menezes da Cunha Júnior <almir.cunha@incognia.com>
2023-04-08 21:52:16 -0300

    fix(plane): treat parallel ray

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -1,10 +1,16 @@
 #include "Plane.hpp"
+#include "constants.hpp"
 
 Plane::Plane(const Point3& point, const Vec3& normal, const Material& material)
     : m_point(point), m_normal(normal.normalized()), m_material(material) {};
 
 bool Plane::intersect(const Ray& ray, double t_min, HitRecord& hitRecord) const {
-    float t = (m_point - ray.getOrigin()).dot(m_normal) / ray.getDirection().dot(m_normal);
+    double denom = ray.getDirection().dot(m_normal);
+    if (std::abs(denom) < EPSILON) {
+        return false;
+    }
+
+    double t = (m_point - ray.getOrigin()).dot(m_normal) / denom;
     if (t < 0) {
         return false;
     }
